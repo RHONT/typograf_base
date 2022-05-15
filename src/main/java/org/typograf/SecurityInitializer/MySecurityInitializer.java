2599a5758239d15a04d311b5c887bd1703227828
RHONT <rg915i@yandex.ru>
2022-05-15 13:52:30 +0300

    Отменил пока security, не могу вдуплить как его встроить в проект со своей конфигурацией. В сети нахожу как это сделать с Java конфиг или целиком на XML, но как сделать 50 на 50, как у меня не могу понять.
    Работаю над функционалом дальше.

---
+++ src/main/java/org/typograf/SecurityInitializer/MySecurityInitializer.java
@@ -0,0 +1,9 @@
+package org.typograf.SecurityInitializer;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.stereotype.Component;
+
+
+//@Component
+//public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+//}
--- src/main/webapp/WEB-INF/view/SelectedTabelDayPage.jsp
+++ src/main/webapp/WEB-INF/view/SelectedTabelDayPage.jsp
@@ -33,6 +33,8 @@
         </tr>
     </c:forEach>
 
+
+
 </table>
 
 </body>
